b1d4a01ca302f6c117602ff786e3dd9f6c9677c8
Jonathan Carrigan <228126051+JonathanCarrigan@users.noreply.github.com >
2025-08-20 09:34:24 -0500

    comments for reflector cache

--- CarriganCore/Carrigan.Core/ReflectionCaching/ReflectorCache.cs
+++ CarriganCore/Carrigan.Core/ReflectionCaching/ReflectorCache.cs
@@ -2,14 +2,34 @@
 
 namespace Carrigan.Core.ReflectionCaching;
 
+/// <summary>
+/// Simple static caching mechanism for frequently used reflections.
+/// This is needed primarily for the Carrigan.SqlTOols, and other libraries
+/// </summary>
+/// <typeparam name="T">the data type your are reflecting</typeparam>
 public static class ReflectorCache<T>
 {
+    /// <summary>
+    /// The type, readonly static
+    /// </summary>
     public readonly static Type Type = typeof(T);
+    /// <summary>
+    /// Public static getter for all public instance CanRead properties, using LazyLoad
+    /// </summary>
     public static IEnumerable<PropertyInfo> ReadablePublicInstanceProperties => _LazyPublicInstanceProperties.Value.Where(property => property.CanRead);
+    /// <summary>
+    /// Public static getter for all public instance CanWrite properties, using LazyLoad
+    /// </summary>
     public static IEnumerable<PropertyInfo> WriteablePublicInstanceProperties => _LazyPublicInstanceProperties.Value.Where(property => property.CanWrite);
-    
 
+    /// <summary>
+    /// Public static getter for all public instance CanWrite properties, using LazyLoad
+    /// </summary>
     private static readonly Lazy<IEnumerable<PropertyInfo>> _LazyPublicInstanceProperties;
+
+    /// <summary>
+    /// Static constructor
+    /// </summary>
     static ReflectorCache()
     {
 
