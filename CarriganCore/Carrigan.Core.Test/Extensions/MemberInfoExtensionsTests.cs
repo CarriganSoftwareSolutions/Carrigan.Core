d74cfcd3f1b60a6b82f33523f8aa363cbc2ae86b
Jonathan Carrigan <228126051+JonathanCarrigan@users.noreply.github.com >
2025-10-04 23:20:52 -0500

    added extension method to wrap logic to get fully qualified member information using reflection.

---
+++ CarriganCore/Carrigan.Core.Test/Extensions/MemberInfoExtensionsTests.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Reflection;
+using Xunit;
+using Carrigan.Core.Extensions;
+
+namespace Carrigan.Core.Test.Extensions;
+
+public class MemberInfoExtensionsTests
+{
+    private class SampleClass
+    {
+        public int Property { get; set; }
+        public void Method() { }
+        public int Field;
+    }
+
+    [Fact]
+    public void GetQualifiedName_ReturnsFullName_ForType()
+    {
+        Type type = typeof(SampleClass);
+
+        string result = type.GetQualifiedName();
+
+        string expected = "Carrigan.Core.Test.Extensions.MemberInfoExtensionsTests+SampleClass";
+        Assert.Equal(expected, result);
+    }
+
+    [Fact]
+    public void GetQualifiedName_ReturnsQualifiedName_ForProperty()
+    {
+        PropertyInfo property = typeof(SampleClass).GetProperty("Property")!;
+
+        string result = property.GetQualifiedName();
+
+        string expected = "Carrigan.Core.Test.Extensions.MemberInfoExtensionsTests+SampleClass.Property";
+        Assert.Equal(expected, result);
+    }
+
+    [Fact]
+    public void GetQualifiedName_ReturnsQualifiedName_ForField()
+    {
+        FieldInfo field = typeof(SampleClass).GetField("Field")!;
+
+        string result = field.GetQualifiedName();
+
+        string expected = "Carrigan.Core.Test.Extensions.MemberInfoExtensionsTests+SampleClass.Field";
+        Assert.Equal(expected, result);
+    }
+
+    [Fact]
+    public void GetQualifiedName_ReturnsQualifiedName_ForMethod()
+    {
+        MethodInfo method = typeof(SampleClass).GetMethod("Method")!;
+
+        string result = method.GetQualifiedName();
+
+        string expected = "Carrigan.Core.Test.Extensions.MemberInfoExtensionsTests+SampleClass.Method";
+        Assert.Equal(expected, result);
+    }
+}
---
+++ CarriganCore/Carrigan.Core/Extensions/MemberInfoExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+
+namespace Carrigan.Core.Extensions;
+public static class MemberInfoExtensions
+{
+    public static string GetQualifiedName(this MemberInfo member)
+    {
+        if (member is Type type)
+            return type.FullName ?? type.Name;
+
+        string declaring = member.DeclaringType?.FullName ?? "<global>";
+        return $"{declaring}.{member.Name}";
+    }
+}
