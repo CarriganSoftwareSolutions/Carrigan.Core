4a5fb7521fab210275d0ebdf2ea2a43da2b588b6
Jonathan Carrigan <228126051+JonathanCarrigan@users.noreply.github.com >
2026-02-18 06:28:19 -0600

    added a class to represent bootstrap breakpoints

---
+++ CarriganCore/Carrigan.Core.Test/DataTypeTests/BootstrapBreakPointsTests.cs
@@ -0,0 +1,90 @@
+﻿using Carrigan.Core.DataTypes;
+using Xunit;
+
+namespace Carrigan.Core.Test.DataTypeTests;
+
+public sealed class BootstrapBreakPointsTests
+{
+    [Fact]
+    public void ToHtml_WhenAllValuesAreNull_ReturnsColAutoWithSpaces()
+    {
+        BootstrapBreakPoints sut = new();
+
+        string actual = sut.ToHtml();
+
+        Assert.Equal(" col-auto ", actual);
+    }
+
+    [Fact]
+    public void ToHtml_WhenOnlyColIsSet_ReturnsColNWithLeadingAndTrailingSpaces()
+    {
+        BootstrapBreakPoints sut = new()
+        {
+            Col = 6
+        };
+
+        string actual = sut.ToHtml();
+
+        Assert.Equal(" col-6 ", actual);
+    }
+
+    [Fact]
+    public void ToHtml_WhenOnlySmIsSet_ReturnsOnlySmClassWithLeadingAndTrailingSpaces()
+    {
+        BootstrapBreakPoints sut = new()
+        {
+            Sm = 4
+        };
+
+        string actual = sut.ToHtml();
+
+        Assert.Equal(" col-sm-4 ", actual);
+    }
+
+    [Fact]
+    public void ToHtml_WhenMultipleBreakpointsAreSet_ReturnsAllInCorrectOrderWithSingleSpaces()
+    {
+        BootstrapBreakPoints sut = new()
+        {
+            Col = 12,
+            Sm = 6,
+            Md = 4,
+            Lg = 3,
+            Xl = 2,
+            Xxl = 1
+        };
+
+        string actual = sut.ToHtml();
+
+        Assert.Equal(" col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2 col-xxl-1 ", actual);
+    }
+
+    [Fact]
+    public void ToHtml_WhenSomeBreakpointsAreNull_OmitsThem()
+    {
+        BootstrapBreakPoints sut = new()
+        {
+            Col = 12,
+            Md = 6,
+            Xxl = 3
+        };
+
+        string actual = sut.ToHtml();
+
+        Assert.Equal(" col-12 col-md-6 col-xxl-3 ", actual);
+    }
+
+    [Fact]
+    public void ToHtml_WhenColIsNullButOthersAreSet_DoesNotReturnColAuto()
+    {
+        BootstrapBreakPoints sut = new()
+        {
+            Sm = 12
+        };
+
+        string actual = sut.ToHtml();
+
+        Assert.NotEqual(" col-auto ", actual);
+        Assert.Equal(" col-sm-12 ", actual);
+    }
+}
---
+++ CarriganCore/Carrigan.Core/DataTypes/BootstrapBreakPoints.cs
@@ -0,0 +1,86 @@
+﻿using System.Text;
+
+namespace Carrigan.Core.DataTypes;
+
+public class BootstrapBreakPoints
+{
+    public int? Col { get; set; }
+    public int? Sm { get; set; }
+    public int? Md { get; set; }
+    public int? Lg { get; set; }
+    public int? Xl { get; set; }
+    public int? Xxl { get; set; }
+
+
+    /// <summary>
+    /// Builds Bootstrap grid classes for the configured breakpoints.
+    /// - If all values are null, returns " col-auto ".
+    /// - Otherwise returns a string that begins and ends with a space, containing:
+    ///   - Col  -> "col-{n}"
+    ///   - Sm   -> "col-sm-{n}"
+    ///   - Md   -> "col-md-{n}"
+    ///   - Lg   -> "col-lg-{n}"
+    ///   - Xl   -> "col-xl-{n}"
+    ///   - Xxl  -> "col-xxl-{n}"
+    /// - Any null breakpoint is omitted.
+    /// </summary>
+    public string ToHtml()
+    {
+        if (Col is null  && Sm is null && Md is null && Lg is null && Xl is null && Xxl is null)
+        {
+            return " col-auto ";
+        }
+        else
+        {
+            StringBuilder stringBuilder = new();
+            stringBuilder.Append(' ');
+
+            bool appendedAny = false;
+
+            void AppendClass(string cssClass)
+            {
+                if (appendedAny)
+                {
+                    stringBuilder.Append(' ');
+                }
+
+                stringBuilder.Append(cssClass);
+                appendedAny = true;
+            }
+
+            // Col corresponds to the default "col-{n}" form
+            if (Col is not null)
+            {
+                AppendClass($"col-{Col.Value}");
+            }
+
+            if (Sm is not null)
+            {
+                AppendClass($"col-sm-{Sm.Value}");
+            }
+
+            if (Md is not null)
+            {
+                AppendClass($"col-md-{Md.Value}");
+            }
+
+            if (Lg is not null)
+            {
+                AppendClass($"col-lg-{Lg.Value}");
+            }
+
+            if (Xl is not null)
+            {
+                AppendClass($"col-xl-{Xl.Value}");
+            }
+
+            if (Xxl is not null)
+            {
+                AppendClass($"col-xxl-{Xxl.Value}");
+            }
+
+            stringBuilder.Append(' ');
+            return stringBuilder.ToString();
+        }
+    }
+}
