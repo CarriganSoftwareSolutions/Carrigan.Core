744a6912fb96bd1653e362fb477746527a1d775f
Jonathan Carrigan <228126051+JonathanCarrigan@users.noreply.github.com >
2026-03-05 10:58:15 -0600

    no message

---
+++ CarriganCore/Carrigan.Core/Enums/Html/Bootstrap/BreakPointsEnum.cs
@@ -0,0 +1,11 @@
+﻿namespace Carrigan.Core.Enums.Html.Bootstrap;
+
+public enum BreakPointsEnum
+{
+    Default = 0,
+    Small = 1,
+    Medium = 2,
+    Large = 3,
+    ExtraLarge = 4,
+    ExtraExtraLarge = 5
+}
