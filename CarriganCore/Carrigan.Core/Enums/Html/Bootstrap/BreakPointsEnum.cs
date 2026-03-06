268a6590d6c6d27e698bb98bf39a8f195a6e6294
Jonathan Carrigan <228126051+JonathanCarrigan@users.noreply.github.com >
2026-03-05 21:24:17 -0600

    added break point size method

--- CarriganCore/Carrigan.Core/Enums/Html/Bootstrap/BreakPointsEnum.cs
+++ CarriganCore/Carrigan.Core/Enums/Html/Bootstrap/BreakPointsEnum.cs
@@ -1,4 +1,6 @@
-﻿namespace Carrigan.Core.Enums.Html.Bootstrap;
+﻿using Carrigan.Core.DataTypes;
+
+namespace Carrigan.Core.Enums.Html.Bootstrap;
 
 public enum BreakPointsEnum
 {
@@ -8,4 +10,25 @@ public enum BreakPointsEnum
     Large = 3,
     ExtraLarge = 4,
     ExtraExtraLarge = 5
+}
+
+public static class BreakPointsEnumExtensions
+{
+    public static int GetColWidthAtBreakpoint(this BreakPointsEnum at, BootstrapBreakPoints breakPoint, int defaultWidth = 4)
+    {
+        ArgumentNullException.ThrowIfNull(breakPoint);
+
+        int? pick = at switch
+        {
+            BreakPointsEnum.Small => breakPoint.Sm,
+            BreakPointsEnum.Medium => breakPoint.Md,
+            BreakPointsEnum.Large => breakPoint.Lg,
+            BreakPointsEnum.ExtraLarge => breakPoint.Xl,
+            BreakPointsEnum.ExtraExtraLarge => breakPoint.Xxl,
+            _ => breakPoint.Col
+        };
+
+        int width = pick ?? breakPoint.Col ?? defaultWidth;
+        return width < 1 ? 1 : (width > 12 ? 12 : width);
+    }
 }
