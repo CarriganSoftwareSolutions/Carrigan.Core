dac199857bc2101d64afff238f735a79cfc85782
Jonathan Carrigan <228126051+JonathanCarrigan@users.noreply.github.com>
2026-08-20 00:12:56 -0500

    Expanding the types of class members that ExternalOnly can be applied to.

--- CarriganCore/Carrigan.Core.Analyzers/ExternalOnlyAnalyzer.cs
+++ CarriganCore/Carrigan.Core.Analyzers/ExternalOnlyAnalyzer.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections.Immutable;
-using System.Linq;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.Diagnostics;
 using Microsoft.CodeAnalysis.Operations;
@@ -9,11 +8,11 @@ namespace Carrigan.Core.Analyzers;
 [DiagnosticAnalyzer(LanguageNames.CSharp)]
 public sealed class ExternalOnlyAnalyzer : DiagnosticAnalyzer
 {
-    public const string DiagnosticId = "CARRIGAN0001";
-    private const string MessageFormat = "Member '{0}' is marked [ExternalOnly] and should not be called from within assembly '{1}'";
+    public const string DiagnosticId = "CARRIGAN0001"; 
+    private const string MessageFormat = "Member '{0}' is marked [ExternalOnly] and should not be used from within assembly '{1}'";
     private static readonly DiagnosticDescriptor Rule = new (
         id: DiagnosticId,
-        title: "External-only API invoked internally",
+        title: "External-only API used internally",
         messageFormat: MessageFormat,
         category: "Usage",
         defaultSeverity: DiagnosticSeverity.Error,
@@ -34,62 +33,93 @@ public sealed class ExternalOnlyAnalyzer : DiagnosticAnalyzer
         context.EnableConcurrentExecution();
         context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
 
-        context.RegisterOperationAction(AnalyzeInvocation, OperationKind.Invocation);
-        context.RegisterOperationAction(AnalyzeObjectCreation, OperationKind.ObjectCreation);
+        context.RegisterOperationAction(
+            AnalyzeOperation,
+            OperationKind.Invocation,
+            OperationKind.ObjectCreation,
+            OperationKind.PropertyReference,
+            OperationKind.FieldReference,
+            OperationKind.EventReference,
+            OperationKind.MethodReference,
+            OperationKind.Binary,
+            OperationKind.Unary,
+            OperationKind.Conversion,
+            OperationKind.Increment,
+            OperationKind.Decrement,
+            OperationKind.CompoundAssignment);
     }
 
-    private static bool HasExternalOnly(ISymbol symbol)
+    private static void AnalyzeOperation(OperationAnalysisContext context)
     {
-        foreach (AttributeData attribute in symbol.GetAttributes())
+        if (GetReferencedSymbol(context.Operation) is ISymbol symbol)
         {
-            INamedTypeSymbol? attributeClass = attribute.AttributeClass;
-            if (attributeClass is not null)
+            if (HasExternalOnly(symbol) && IsInternalCall(context.Compilation, symbol))
             {
-                if (attributeClass.Name == "ExternalOnlyAttribute"
-                    || attributeClass
-                        .ToDisplayString(SymbolDisplayFormat.FullyQualifiedFormat)
-                        .EndsWith(".ExternalOnlyAttribute"))
-                {
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
+                Location location = context.Operation.Syntax.GetLocation();
+                string displayString = symbol.ToDisplayString(SymbolDisplayFormat.MinimallyQualifiedFormat);
+                string containingAssemblyName = symbol.ContainingAssembly?.Name ?? "<unknown>";
 
-    private static void AnalyzeObjectCreation(OperationAnalysisContext context)
-    {
-        IObjectCreationOperation operation = (IObjectCreationOperation)context.Operation;
-        IMethodSymbol? constructor = operation.Constructor;
-        if (constructor is not null)
-        {
-            if (HasExternalOnly(constructor) && IsInternalCall(context.Compilation, constructor))
-            {
-                Diagnostic diagnostic = Diagnostic.Create(
-                    Rule,
-                    operation.Syntax.GetLocation(),
-                    constructor.ToDisplayString(SymbolDisplayFormat.MinimallyQualifiedFormat),
-                    constructor.ContainingAssembly?.Name ?? "<unknown>");
+                Diagnostic diagnostic = Diagnostic.Create(Rule, location, displayString, containingAssemblyName);
+
                 context.ReportDiagnostic(diagnostic);
             }
         }
     }
 
-    private static void AnalyzeInvocation(OperationAnalysisContext context)
-    {
-        IInvocationOperation operation = (IInvocationOperation)context.Operation;
-        IMethodSymbol target = operation.TargetMethod;
-
-        if (HasExternalOnly(target) && IsInternalCall(context.Compilation, target))
+    private static ISymbol? GetReferencedSymbol(IOperation operation) =>
+        operation switch
         {
-            Diagnostic diagnostic = Diagnostic.Create(
-                Rule,
-                operation.Syntax.GetLocation(),
-                target.ToDisplayString(SymbolDisplayFormat.MinimallyQualifiedFormat),
-                target.ContainingAssembly?.Name ?? "<unknown>");
-            context.ReportDiagnostic(diagnostic);
+            IInvocationOperation invocation =>
+                invocation.TargetMethod,
+
+            IObjectCreationOperation objectCreation =>
+                objectCreation.Constructor,
+
+            IPropertyReferenceOperation propertyReference =>
+                 propertyReference.Property,
+
+            IFieldReferenceOperation fieldReference =>
+                fieldReference.Field,
+
+            IEventReferenceOperation eventReference =>
+                 eventReference.Event,
+
+            IMethodReferenceOperation methodReference =>
+                 methodReference.Method,
+
+            IBinaryOperation binary =>
+                binary.OperatorMethod,
+
+            IUnaryOperation unary =>
+                 unary.OperatorMethod,
+
+            IConversionOperation conversion =>
+                 conversion.OperatorMethod,
+
+            IIncrementOrDecrementOperation incrementOrDecrement =>
+                 incrementOrDecrement.OperatorMethod,
+
+            ICompoundAssignmentOperation compoundAssignment =>
+                compoundAssignment.OperatorMethod,
+
+
+            _ =>
+                null
+        };
+
+    private static bool HasExternalOnly(ISymbol symbol)
+    {
+        foreach (AttributeData attribute in symbol.GetAttributes())
+        {
+            if (attribute.AttributeClass is INamedTypeSymbol attributeClass)
+            {
+                if (attributeClass.Name == "ExternalOnlyAttribute")
+                    return true;
+            }
         }
+        return false;
     }
+
 
     private static bool IsInternalCall(Compilation compilation, ISymbol callee)
     {
